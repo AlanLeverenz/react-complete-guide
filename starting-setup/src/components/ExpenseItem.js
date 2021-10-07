0965f0cf59cb114b40bd1bd3cc3151c92735770f
Alan Leverenz <awleverenz@aol.com>
2021-10-07 16:39:05 -0400

    added css

--- starting-setup/src/components/ExpenseItem.js
+++ starting-setup/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>October 6, 2021</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>97.51</div>
+      <div className="expense-item__description">
+        <h2>Uber Trip</h2>
+        <div className="expense-item__price">$97.51</div>
       </div>
     </div>
   );
