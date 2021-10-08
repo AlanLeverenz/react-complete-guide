d7ff8ca1e27d4d9b36db0443b29e54f53d466e6a
Alan Leverenz <awleverenz@aol.com>
2021-10-08 17:15:20 -0400

    inserted js and dynamic objects

--- starting-setup/src/components/ExpenseItem.js
+++ starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 9, 8);
+  const expenseTitle = 'Uber Trip';
+  const expenseAmount = 94.37;
+
   return (
     <div className="expense-item">
-      <div>October 6, 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Uber Trip</h2>
-        <div className="expense-item__price">$97.51</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   );
