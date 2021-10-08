7a0b3a2b1638e89ceee9472e17f9ccbfd5b52b02
Alan Leverenz <awleverenz@aol.com>
2021-10-08 17:41:44 -0400

    added props from App

--- starting-setup/src/components/ExpenseItem.js
+++ starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 9, 8);
-  const expenseTitle = 'Uber Trip';
-  const expenseAmount = 94.37;
+function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
