b24aed6de01c22f1ef8a8028434ee915b79b530a
Alan Leverenz <awleverenz@aol.com>
2021-10-17 22:01:55 -0400

    setup child-parent prop relationship with form in the child

--- starting-setup/src/components/NewExpense/NewExpense.js
+++ starting-setup/src/components/NewExpense/NewExpense.js
@@ -4,9 +4,20 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = () => {
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    console.log(expenseData);
+  }
+
+  // saveExpenseDataHandler is used as a pointer to the function
+  // it is passed to the ExpenseForm by the prop onSaveExpenseData
   return (
     <div className='new-expense'>
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
