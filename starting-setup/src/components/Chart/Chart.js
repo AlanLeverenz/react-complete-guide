4ae1a1e42a96041a99a3434cc4646f0655e7d706
Alan Leverenz <awleverenz@aol.com>
2021-10-27 21:07:48 -0400

    added ExpenseChart to view month labels and values as bars

--- starting-setup/src/components/Chart/Chart.js
+++ starting-setup/src/components/Chart/Chart.js
@@ -5,15 +5,19 @@ import './Chart.css';
 
 const Chart = props => {
 
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+
+  // Math.max expects a list, which the spread operator creates
+  const totalMaximum = Math.max(...dataPointValues);
+
   return <div className="chart">
 
     {props.dataPoints.map(dataPoint => (
       <ChartBar
         key={dataPoint.label}
         value={dataPoint.value}
-        maxValue={null}
+        maxValue={totalMaximum}
         label={dataPoint.label} />))}
-
   </div>
 
 };
--- starting-setup/src/components/Expenses/ExpensesChart.js
+++ starting-setup/src/components/Expenses/ExpensesChart.js
@@ -3,7 +3,32 @@ import React from 'react';
 import Chart from '../Chart/Chart';
 
 const ExpensesChart = props => {
-  return <Chart />
+
+  // setup dataPoint objects for each month
+  const chartDataPoints = [
+    { label: 'Jan', value: 0 },
+    { label: 'Feb', value: 0 },
+    { label: 'Mar', value: 0 },
+    { label: 'Apr', value: 0 },
+    { label: 'May', value: 0 },
+    { label: 'Jun', value: 0 },
+    { label: 'Jul', value: 0 },
+    { label: 'Aug', value: 0 },
+    { label: 'Sep', value: 0 },
+    { label: 'Oct', value: 0 },
+    { label: 'Nov', value: 0 },
+    { label: 'Dec', value: 0 },
+  ];
+
+  // for of loop used because props.expenses is an object, not an array
+  for (const expense of props.expenses) {
+    const expenseMonth = expense.date.getMonth();
+    // starts with Jan = 0, month is the index of the array
+    chartDataPoints[expenseMonth].value += expense.amount;
+    // increases the amount per month 
+  }
+
+  return <Chart dataPoints={chartDataPoints} />
 };
 
 export default ExpensesChart;
