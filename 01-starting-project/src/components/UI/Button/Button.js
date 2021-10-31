205afd64f27fb31297b7efcf8ef02ec13e752eef
Alan Leverenz <awleverenz@aol.com>
2021-10-31 12:03:19 -0400

    inserted css content into styled.button method

--- 01-starting-project/src/components/UI/Button/Button.js
+++ 01-starting-project/src/components/UI/Button/Button.js
@@ -1,13 +1,39 @@
-import React from 'react';
+// import React from 'react'; removed React since there is no JSX
+import styled from 'styled-components';
 
-import './Button.css';
+// import './Button.css';
+// moved css contents into the styled.button method
+// & is a replacement for '.button' 
+// button is removed as the className since it is the styled method
+const Button = styled.button`
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+&:focus {
+  outline: none;
+}
+
+&:hover,
+&:active {
+  background: #ac0e77;
+  border-color: #ac0e77;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+}
+
+`;  // tagged template literal, button is a method
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
