c61e73839621f1ef566299ce0f28676a3d019d6e
Alan Leverenz <awleverenz@aol.com>
2021-11-01 17:44:29 -0400

    added styled component to CourseInput

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,44 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+// remove selectors in a styled div (styled doesn't need them)
+// and use pseudo selectors for the classes with tags, ids, classes)
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  &:label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  &:input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  &:input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &:invalid input {
+    border-color: red;
+    background: rgb(252, 175, 175)
+  }
+
+  &:invalid label {
+    color: red;
+  }
+`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
