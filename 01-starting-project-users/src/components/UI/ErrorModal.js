856e3a8e78dd834fe608fb4d4f03289fc880c51e
Alan Leverenz <awleverenz@aol.com>
2021-11-10 17:34:35 -0500

    adding error modal

---
+++ 01-starting-project-users/src/components/UI/ErrorModal.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Card from './Card';
+import './Error.module.css';
+
+const ErrorModal = props => {
+  return <Card>
+    <header>
+      <h2>You Screwed Up!</h2>
+    </header>
+    <div>
+      <p></p>
+    </div>
+    <footer>
+
+    </footer>
+  </Card>
+};
+
+export default ErrorModal;
+
