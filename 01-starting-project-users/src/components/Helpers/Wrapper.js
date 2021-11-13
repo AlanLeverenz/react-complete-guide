69caaa2b986c24d1d05ca6d1465924a0af2fc214
Alan Leverenz <awleverenz@aol.com>
2021-11-12 21:09:35 -0500

    wrappers

---
+++ 01-starting-project-users/src/components/Helpers/Wrapper.js
@@ -0,0 +1,6 @@
+const Wrapper = props => {
+  return props.children;
+};
+
+// empty component. passes all tags and content between Wrapper tag
+export default Wrapper;
--- 01-starting-project-users/src/components/Users/AddUser.js
+++ 01-starting-project-users/src/components/Users/AddUser.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
-import classes from './AddUser.module.css';
 import Button from '../UI/Button';
+import Wrapper from '../Helpers/Wrapper';
+import classes from './AddUser.module.css';
 
 const AddUser = props => {
   // array destructuring
@@ -47,7 +48,7 @@ const AddUser = props => {
   }
 
   return (
-    <div>
+    <Wrapper>
       {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
@@ -66,7 +67,7 @@ const AddUser = props => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 
 };
