a95895b8345e1fb147676d483a13bbf743bf4ce4
Alan Leverenz <awleverenz@aol.com>
2021-11-18 23:58:58 -0500

    setup useEffect so only runs once if dependencies unchanged

--- 01-starting-project-emailpwd/src/App.js
+++ 01-starting-project-emailpwd/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -8,11 +8,15 @@ function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+  // runs once after app starts
+  // only runs if the array of dependencies have changed
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
 
-  if (storedUserLoggedInInformation === '1') {
-    setIsLoggedIn(true);
-  }
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
