ce4b921e20d06af92892a69602b2758dc09136fb
Alan Leverenz <awleverenz@aol.com>
2022-02-17 21:14:36 -0500

    inserted useEffect in Input.js

--- 01-starting-project-emailpwd/src/components/UI/Input/Input.js
+++ 01-starting-project-emailpwd/src/components/UI/Input/Input.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import classes from './Input.module.css';
 
 const Input = props => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
     <div
       className={`${classes.control} ${props.isValid === false ? classes.invalid : ''
@@ -10,6 +16,7 @@ const Input = props => {
     >
       <label htmlFor="{props.id}">{props.label}</label>
       <input
+        ref={inputRef}
         type="{props.type}"
         id="{props.id}"
         value={props.value}
