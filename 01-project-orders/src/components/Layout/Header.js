cfd902595774a0533764ba9087e4aac107def623
Alan Leverenz <awleverenz@aol.com>
2022-02-19 14:51:43 -0500

    corrected jpeg file path

--- 01-project-orders/src/components/Layout/Header.js
+++ 01-project-orders/src/components/Layout/Header.js
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 
-import mealsImage from '../../assets/meals.jpg';
+import mealsImage from '../../assets/meals.jpeg';
 import classes from './Header.module.css';
 
 const Header = props => {
