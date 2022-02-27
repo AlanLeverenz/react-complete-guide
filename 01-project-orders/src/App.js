f72bd120b466e87b8864360f832fe9cd5be25349
Alan Leverenz <awleverenz@aol.com>
2022-02-27 13:21:06 -0500

    updated App with CartProvider

--- 01-project-orders/src/App.js
+++ 01-project-orders/src/App.js
@@ -1,8 +1,9 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -18,13 +19,13 @@ function App() {
   // passing showCartHandler pointer to the Header as a prop
   // passing hideCartHandler point to the Cart as a prop
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
