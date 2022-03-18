4e2d27b6db5a7b4ede5631112490831901faa23b
Alan Leverenz <awleverenz@aol.com>
2022-03-18 16:41:10 -0400

    added second button to effect closure

--- 01-starting-project-DOM-updates/src/App.js
+++ 01-starting-project-DOM-updates/src/App.js
@@ -6,25 +6,34 @@ import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  }, []);
+    if (allowToggle) {
+      setShowParagraph(prevShowParagraph => !prevShowParagraph);
+    }
+    // overrides useCallBack using a dependency
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  }
 
   // empty array dependencies. always the same 
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
 
 export default App;
-// useCallback stores the function so it is not re-rendered
+// useCallback stores the function so it is not re-rendered...
 // unless the inputs change (memoization)
 // <DemoOutput show={showParagraph} />
