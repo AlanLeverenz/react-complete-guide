7184ce3b6b9f5f9c9941d7c968599def2324dc5f
Alan Leverenz <awleverenz@aol.com>
2022-03-23 21:48:59 -0400

    converted User component from function to class

--- 01-starting-project-class-users/src/components/User.js
+++ 01-starting-project-class-users/src/components/User.js
@@ -1,7 +1,18 @@
+import { Component } from 'react';
+
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// render method doesn't receive props
+// class extends the Component class, inherits it
+// 'this' refers to the class data received for this component
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
