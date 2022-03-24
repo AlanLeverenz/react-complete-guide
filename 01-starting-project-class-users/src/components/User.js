6f705480c2644bc3ec6fbd571dc52a1d4b5bed8f
Alan Leverenz <awleverenz@aol.com>
2022-03-23 21:50:31 -0400

    updated comments

--- 01-starting-project-class-users/src/components/User.js
+++ 01-starting-project-class-users/src/components/User.js
@@ -5,6 +5,8 @@ import classes from './User.module.css';
 // render method doesn't receive props
 // class extends the Component class, inherits it
 // 'this' refers to the class data received for this component
+// you can mix and match class and function components
+
 class User extends Component {
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
