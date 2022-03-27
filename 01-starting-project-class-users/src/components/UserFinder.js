fe63965484cad629ef0558c1690d62304afc3b7b
Alan Leverenz <awleverenz@aol.com>
2022-03-26 23:20:54 -0400

    updated UserFinder to include its css module

--- 01-starting-project-class-users/src/components/UserFinder.js
+++ 01-starting-project-class-users/src/components/UserFinder.js
@@ -1,6 +1,12 @@
 import { Fragment, useState, useEffect } from 'react';
-
+import classes from './UserFinder.module.css';
 import Users from './Users';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
 
 const UserFinder = () => {
   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
@@ -18,7 +24,7 @@ const UserFinder = () => {
 
   return (
     <Fragment>
-      <input type='search' onChange={searchChangeHandler} />
+      <input className={classes.finder} type='search' onChange={searchChangeHandler} />
       <Users users={filteredUsers} />
     </Fragment>
   );
