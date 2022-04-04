75a781aa530e6a760e282cc6d3cbafb56cfc61cf
Alan Leverenz <awleverenz@aol.com>
2022-04-04 07:32:14 -0400

    added isLoading state

--- 01-starting-project-http/src/App.js
+++ 01-starting-project-http/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // function fetchMoviesHandler() {
   //   fetch('https://swapi.dev/api/films/')
@@ -14,6 +15,7 @@ function App() {
   // .then(data => {
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
 
@@ -26,6 +28,7 @@ function App() {
       }
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -34,7 +37,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
