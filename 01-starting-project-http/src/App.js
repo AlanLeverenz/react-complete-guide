a01760a94f9b8f5a9c2e8e6b61b603b733ee7808
Alan Leverenz <awleverenz@aol.com>
2022-04-06 23:01:11 -0400

    added Firebase node for storing movies

--- 01-starting-project-http/src/App.js
+++ 01-starting-project-http/src/App.js
@@ -13,7 +13,9 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-7465d-default-rtdb.firebaseio.com/movies.json');
+      // need .json to succeed
+      // dynamic REST api (not a database)
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
