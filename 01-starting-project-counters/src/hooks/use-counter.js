fddf50937cba7d79112cda8b21ceb489058dbb86
Alan Leverenz <awleverenz@aol.com>
2022-04-08 09:57:08 -0400

    refactored the custom hook

--- 01-starting-project-counters/src/hooks/use-counter.js
+++ 01-starting-project-counters/src/hooks/use-counter.js
@@ -3,16 +3,20 @@ import { useState, useEffect } from 'react';
 // must start a custom hook with "use"
 // the logic of an imported custom hook is shared, but not the state
 
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
-
+    // forwards is parameter that is a dependency 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
 
   return counter;
 };
