b616c59741709bf4eb149b98ba988510bd3f11a4
Alan Leverenz <awleverenz@aol.com>
2022-05-22 20:48:40 -0400

    added index.js to store folder

---
+++ 01-starting-project-redux-counter/src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from 'redux';
+
+// action types stored in counterReducer
+// redux store uses them when an action is dispatched by the subscriber
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1,
+    }
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    }
+  }
+
+  return state;
+
+};
+
+const store = createStore(counterReducer);
+
+export default store;
