6763afcd8cd62997d62ec0f56368503d94b227d0
Alan Leverenz <awleverenz@aol.com>
2022-05-24 10:14:59 -0400

    updated Counter component with useSelector

--- 01-starting-project-redux-counter/src/components/Counter.js
+++ 01-starting-project-redux-counter/src/components/Counter.js
@@ -1,12 +1,20 @@
+import { useSelector } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+
+  // useSelector gets specific state values from the store
+  // redux automatically sets up a subscription to the store
+  // component is updated whenever state changes
+  const counter = useSelector(state => state.counter);
+
+  const toggleCounterHandler = () => { };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
