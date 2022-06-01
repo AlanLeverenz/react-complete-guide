7201a979ba1ab98b6982087266b26b2be1cd52fd
Alan Leverenz <awleverenz@aol.com>
2022-05-31 20:58:15 -0400

    added authSlice to store index file

--- 01-starting-project-redux-counter/src/store/index.js
+++ 01-starting-project-redux-counter/src/store/index.js
@@ -1,7 +1,7 @@
 // import { createStore } from 'redux';
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 // can identify parts of the overall state
 // redux toolkit will include the whole state for the update
@@ -9,7 +9,7 @@ const initialState = { counter: 0, showCounter: true };
 // redux toolkit fixes it under the hood
 const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -18,10 +18,29 @@ const counterSlice = createSlice({
       state.counter--;
     },
     increase(state, action) {
+      // action.payload carries the payload from Counter
       state.counter = state.counter + action.payload;
     },
     toggle(state) {
       state.showCounter = !state.showCounter;
+    }
+  }
+});
+
+// AUTH STATE SLICE
+const initialAuthState = {
+  isAuthenticated: false
+}
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthetnicated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
     }
   }
 });
@@ -73,11 +92,16 @@ const counterSlice = createSlice({
 // });
 
 
+// map of reducers
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer
+  },
 });
 
 // creates a unique ID for each action object created in counterSlice
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
