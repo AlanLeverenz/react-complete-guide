91b8a6f667a019fe7338d22184e248360c07d842
Alan Leverenz <awleverenz@aol.com>
2022-06-01 21:20:57 -0400

    added authSlice file with authSlice

--- 01-starting-project-redux-counter/src/store/index.js
+++ 01-starting-project-redux-counter/src/store/index.js
@@ -1,49 +1,7 @@
 // import { createStore } from 'redux';
-import { createSlice, configureStore } from '@reduxjs/toolkit';
-
-// COUNTER SLICE
-const initialCounterState = { counter: 0, showCounter: true };
-// can identify parts of the overall state
-// redux toolkit will include the whole state for the update
-// can now change state primitive values in createSlice
-// redux toolkit fixes it under the hood
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      // action.payload carries the payload from Counter
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    }
-  }
-});
-
-// AUTH STATE SLICE
-const initialAuthState = {
-  isAuthenticated: false
-}
-
-const authSlice = createSlice({
-  name: 'authentication',
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    }
-  }
-});
+import { configureStore } from '@reduxjs/toolkit';
+import counterSlice from './counter';
+import authSlice from './auth';
 
 // map of reducers
 const store = configureStore({
