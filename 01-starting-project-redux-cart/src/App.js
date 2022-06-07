ceafdf42712a385e3d0c269a245f0832b8bc0d41
Alan Leverenz <awleverenz@aol.com>
2022-06-07 09:49:47 -0400

    edited cart-slice

--- 01-starting-project-redux-cart/src/App.js
+++ 01-starting-project-redux-cart/src/App.js
@@ -1,11 +1,17 @@
+import { useSelector } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+  // gets the precise state object we need
+  // pass a function that receives the redux state automatically
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+
   return (
     <Layout>
-      <Cart />
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
