23704fd01564ca9d38736e65209fa5141ffa960b
Alan Leverenz <awleverenz@aol.com>
2022-06-09 21:12:43 -0400

    corrected cart-slice

--- 01-starting-project-redux-cart/src/store/cart-slice.js
+++ 01-starting-project-redux-cart/src/store/cart-slice.js
@@ -5,15 +5,12 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0,
-    totalAmount: 0
   },
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem = state.items.find(item => item.id === newItem.id);
-      // redux toolkit allows primitive value changes
+      const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
-      // toolkit converts this operation
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -21,28 +18,25 @@ const cartSlice = createSlice({
           quantity: 1,
           totalPrice: newItem.price,
           name: newItem.title
-        })
+        });
       } else {
-        existingItem.totalQuantity++;
-        existingItem.totalPrice = existingItem.totalPrice + newItem.price
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find(item => item.id === id);
-      // redux toolkit allows primitive value changes
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter(item => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
 });
 
-// export actions because they are needed for dispatching
 export const cartActions = cartSlice.actions;
 
 export default cartSlice;
