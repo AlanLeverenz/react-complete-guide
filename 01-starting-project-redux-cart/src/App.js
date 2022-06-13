ace19f31ddcf9c32be703856b3692b86a49dc10a
Alan Leverenz <awleverenz@aol.com>
2022-06-12 21:46:42 -0400

    edited App to fetch fb when cart changes

--- 01-starting-project-redux-cart/src/App.js
+++ 01-starting-project-redux-cart/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -8,6 +9,17 @@ function App() {
   // gets the precise state object we need
   // pass a function that receives the redux state automatically
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  // sets up a subscription to redux
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    // overwrites existing fb data
+    fetch('https://react-http-6b4a6.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+    // cart is the dependency that when changed the fetch function runs
+  }, [cart]);
 
   return (
     <Layout>
