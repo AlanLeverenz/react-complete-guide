7e01f5206ca70181685d60d45151056a2cf57974
Alan Leverenz <awleverenz@aol.com>
2022-06-13 20:31:51 -0400

    updated App with three different http notifications

--- 01-starting-project-redux-cart/src/App.js
+++ 01-starting-project-redux-cart/src/App.js
@@ -1,32 +1,74 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { uiActions } from './store/ui-slice';
 
 function App() {
+  const dispatch = useDispatch();
   // gets the precise state object we need
   // pass a function that receives the redux state automatically
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   // sets up a subscription to redux
   const cart = useSelector(state => state.cart);
 
+  // overwrites existing fb data
+  // storing https request as a separate function to use async await
+  // dispatching action to show notifications of http request
   useEffect(() => {
-    // overwrites existing fb data
-    fetch('https://react-http-7465d-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart),
-    });
-    // cart is the dependency that when changed the fetch function runs
-  }, [cart]);
+    const sendCartData = async () => {
 
-  return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
-  );
-}
+      // initial status before running https request
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
+      );
+
+      const response = await fetch(
+        'https://react-http-7465d-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+
+      // if successful
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sent cart data successfully!',
+        })
+      );
+    };
+
+    // call sendCartData function
+    // cart is the dependency that when changed the fetch function runs
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data failed!',
+        })
+      );
+    }, [cart]);
+
+    return (
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    );
+  }
 
 export default App;
