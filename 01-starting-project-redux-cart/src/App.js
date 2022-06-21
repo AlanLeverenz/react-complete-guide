fd4c21d661812409c89d4ed6f6004542d36da770
Alan Leverenz <awleverenz@aol.com>
2022-06-21 16:49:01 -0400

    added cart-actions for fetching data

--- 01-starting-project-redux-cart/src/App.js
+++ 01-starting-project-redux-cart/src/App.js
@@ -4,8 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-// import { uiActions } from './store/ui-slice';
-import { sendCartData } from './store/cart-slice';
+import { sendCartData, fetchCartData } from './store/cart-actions';
 import Notification from './components/UI/Notification';
 
 let isInitial = true;
@@ -16,8 +15,12 @@ function App() {
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
 
+  // run on initial loading of the page
   useEffect(() => {
-    // const sendCartData = async () => {
+    dispatch(fetchCartData())
+  }, [dispatch]);
+
+  useEffect(() => {
 
     if (isInitial) {
       isInitial = false;
@@ -27,50 +30,6 @@ function App() {
     dispatch(sendCartData(cart));
 
   }, [cart, dispatch]);
-
-  // dispatch(
-  //   uiActions.showNotification({
-  //     status: 'pending',
-  //     title: 'Sending...',
-  //     message: 'Sending cart data!',
-  //   })
-  // );
-  // const response = await fetch(
-  //   'https://react-http-6b4a6.firebaseio.com/cart.json',
-  //   {
-  //     method: 'PUT',
-  //     body: JSON.stringify(cart),
-  //   }
-  // );
-
-  // if (!response.ok) {
-  //   throw new Error('Sending cart data failed.');
-  // }
-
-  // dispatch(
-  //   uiActions.showNotification({
-  //     status: 'success',
-  //     title: 'Success!',
-  //     message: 'Sent cart data successfully!',
-  //   })
-  // );
-  // };
-
-  // if (isInitial) {
-  //   isInitial = false;
-  //   return;
-  // }
-
-  // sendCartData().catch((error) => {
-  // dispatch(
-  //   uiActions.showNotification({
-  //     status: 'error',
-  //     title: 'Error!',
-  //     message: 'Sending cart data failed!',
-  //   })
-  // );
-  // });
-  // }, [cart, dispatch]);
 
   return (
     <Fragment>
