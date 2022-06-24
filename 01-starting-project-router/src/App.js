fddadbdd5de0f7480ef07704068ed0e1818da32f
Alan Leverenz <awleverenz@aol.com>
2022-06-24 09:39:24 -0400

    added MainHeader component

--- 01-starting-project-router/src/App.js
+++ 01-starting-project-router/src/App.js
@@ -1,11 +1,14 @@
+// for client side routing
 import { Route } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import MainHeader from './components/MainHeader';
 
 function App() {
   return (
     <div>
+      <MainHeader />
       <Route path="/welcome">
         <Welcome />
       </Route>
