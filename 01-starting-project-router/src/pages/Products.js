b65419f0ebe2bdd3726703b466889ea5f204223f
Alan Leverenz <awleverenz@aol.com>
2022-06-27 10:22:58 -0400

    added ProductDetail to paths and component files

--- 01-starting-project-router/src/pages/Products.js
+++ 01-starting-project-router/src/pages/Products.js
@@ -1,5 +1,16 @@
+// import ProductDetail from './ProductDetail';
+
 const Products = () => {
-  return <h1>The Products Page</h1>
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </section>
+  )
 }
 
 export default Products;
