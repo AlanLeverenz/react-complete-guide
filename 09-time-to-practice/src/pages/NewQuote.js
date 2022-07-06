a15693f547e8bf709cab9caf16f598fe1fe69e37
Alan Leverenz <awleverenz@aol.com>
2022-07-06 19:09:23 -0400

    added QuoteForm in NewQuote page

--- 09-time-to-practice/src/pages/NewQuote.js
+++ 09-time-to-practice/src/pages/NewQuote.js
@@ -1,6 +1,11 @@
+import QuoteForm from '../components/quotes/QuoteForm';
+
 const NewQuote = () => {
+  const addQuoteHandler = quoteData => {
+    console.log(quoteData);
+  }
   return (
-    <h1>New Quote Page</h1>
+    <QuoteForm onAddQuote={addQuoteHandler} />
   )
 };
 export default NewQuote;
