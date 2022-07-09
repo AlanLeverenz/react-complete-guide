c0364a7a8b97ffa60c363ef2883fd3c6d1f7fed9
Alan Leverenz <awleverenz@aol.com>
2022-07-09 18:09:27 -0400

    added query parameters to QuoteList

--- 09-time-to-practice/src/components/quotes/QuoteList.js
+++ 09-time-to-practice/src/components/quotes/QuoteList.js
@@ -1,11 +1,22 @@
 import { Fragment } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
 const QuoteList = (props) => {
+
+  const history = useHistory();
+
+  const changeSortingHandler = () => {
+    history.push('/quotes?sort=asc');
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort Ascending</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
@@ -16,6 +27,7 @@ const QuoteList = (props) => {
           />
         ))}
       </ul>
+
     </Fragment>
   );
 };
