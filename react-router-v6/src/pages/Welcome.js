2f8a92dd241c319cbdc7effb738186a0a8c23c64
Alan Leverenz <awleverenz@aol.com>
2022-07-17 17:52:02 -0400

    demoed useNavigation, added Outlet for child paths

--- react-router-v6/src/pages/Welcome.js
+++ react-router-v6/src/pages/Welcome.js
@@ -1,15 +1,17 @@
-import { Route, Routes, Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
       <Link to="new-user">New User</Link>
-      <Routes>
-        <Route path="new-user" element={<p>Welcome, new user!</p>} />
-      </Routes>
+      <Outlet />
     </section>
   );
 };
 
 export default Welcome;
+
+// <Routes>
+// <Route path="new-user" element={<p>Welcome, new user!</p>} />
+// </Routes>
