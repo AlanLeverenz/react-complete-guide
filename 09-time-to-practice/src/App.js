878653dad01d8a638c3cda45fd849e575477fee7
Alan Leverenz <awleverenz@aol.com>
2022-07-19 17:51:41 -0400

    edited App comments

--- 09-time-to-practice/src/App.js
+++ 09-time-to-practice/src/App.js
@@ -1,14 +1,9 @@
 import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-// import AllQuotes from './pages/AllQuotes';
-// import QuoteDetail from './pages/QuoteDetail';
-// import NewQuote from './pages/NewQuote';
 import Layout from './components/layout/Layout';
-// import NotFound from './pages/NotFound';
 import LoadingSpinner from './components/UI/LoadingSpinner';
 
-// lazy loading the NewQuote component
 const NewQuote = React.lazy(() => import('./pages/NewQuote'));
 const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
 const NotFound = React.lazy(() => import('./pages/NotFound'));
