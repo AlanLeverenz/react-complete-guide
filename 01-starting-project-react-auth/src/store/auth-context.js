acc62ee9b7d618fbbd53512e917ef12678a267af
Alan Leverenz <awleverenz@aol.com>
2022-07-31 07:54:32 -0400

    save token in localStorage

--- 01-starting-project-react-auth/src/store/auth-context.js
+++ 01-starting-project-react-auth/src/store/auth-context.js
@@ -12,16 +12,19 @@ const AuthContext = React.createContext({
 // setup as a wrapper for child components
 // export as a named const
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null)
+  const initialToken = localStorage.getItem('token');
+  const [token, setToken] = useState(initialToken)
 
   const userIsLoggedIn = !!token; // !! converts truthy to boolean value
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem('token', token);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem('token');
   };
 
   const contextValue = {
