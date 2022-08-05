e33522877513221dfa36c7a0847b483c14dd2f9b
Alan Leverenz <awleverenz@aol.com>
2022-08-05 10:49:02 -0400

    added useRouter for holding concrete values in the URL

--- nextjs-course/pages/news/[newsId].js
+++ nextjs-course/pages/news/[newsId].js
@@ -1,6 +1,15 @@
+import { useRouter } from 'next/router';
+
 // our-domain.com/news/newsId
 
 function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId; // holds the concrete value in the URL
+
+  // send a request to the backend API
+  // to fetch the news item with newsId
+
   return <h1>The Detail Page</h1>
 }
 
