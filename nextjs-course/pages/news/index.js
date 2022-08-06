77a83a740eafb6df3ad3555a6fea854f91f97f1d
Alan Leverenz <awleverenz@aol.com>
2022-08-06 17:26:31 -0400

    added <a> href link to a detail page

--- nextjs-course/pages/news/index.js
+++ nextjs-course/pages/news/index.js
@@ -1,7 +1,19 @@
-// our-domain.com/news
+import { Fragment } from 'react';
 
 function NewsPage() {
-  return <h1>The News Page</h1>
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <a href='/news/nextjs-is-a-great-framework'>NextJs Is a Great Framework
+          </a>
+        </li>
+        <li>Something Else Is Too</li>
+      </ul>
+    </Fragment>
+  )
+
 }
 
 export default NewsPage;
