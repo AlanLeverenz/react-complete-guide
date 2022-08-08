9bc7f2be25362551c4cf70d342de72748ffcdd5f
Alan Leverenz <awleverenz@aol.com>
2022-08-08 11:09:37 -0400

    attempted to load new image

--- 06-nextjs-meetup-project/pages/index.js
+++ 06-nextjs-meetup-project/pages/index.js
@@ -10,7 +10,7 @@ const DUMMY_MEETUPS = [
   },
   {
     id: 'm2',
-    title: 'A First Meetup',
+    title: 'A Second Meetup',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
     address: 'Central Park, New York City',
     description: 'This is a second meetup!'
