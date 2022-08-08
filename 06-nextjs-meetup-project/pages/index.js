090b1f105e90e161b6161c2e929c0af0e9f4b276
Alan Leverenz <awleverenz@aol.com>
2022-08-07 21:41:53 -0400

    setup main index page items

---
+++ 06-nextjs-meetup-project/pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'New Town Hall, Old Town of Munich',
+    description: 'This is a first meetup!'
+  },
+  {
+    id: 'm2',
+    title: 'A First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Central Park, New York City',
+    description: 'This is a second meetup!'
+  }
+]
+
+function HomePage() {
+  return (
+    <MeetupList meetups={DUMMY_MEETUPS} />
+  )
+}
+
+export default HomePage
