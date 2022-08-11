648fcaebf4748210703bce792c76facdb0ebdf5d
Alan Leverenz <awleverenz@aol.com>
2022-08-11 17:31:30 -0400

    updated index.js

--- 06-nextjs-meetup-project/pages/index.js
+++ 06-nextjs-meetup-project/pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -18,8 +19,16 @@ const DUMMY_MEETUPS = [
 ]
 
 function HomePage() {
+
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    // send an http request and fetch data
+    setLoadedMeetups(DUMMY_MEETUPS);
+  }, []);
+
   return (
-    <MeetupList meetups={DUMMY_MEETUPS} />
+    <MeetupList meetups={loadedMeetups} />
   )
 }
 
