49124f1150b737c5664982da8c75479e4a843345
Alan Leverenz <awleverenz@aol.com>
2022-08-15 09:40:18 -0400

    added getStatcProps to meetupId

--- 06-nextjs-meetup-project/pages/[meetupId]/index.js
+++ 06-nextjs-meetup-project/pages/[meetupId]/index.js
@@ -11,5 +11,25 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps(context) {
+  // fetch data from the server for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        id: meetupId,
+        title: 'A First Meetup',
+        address: 'New Town Hall, Old Town of Munich',
+        description: 'This is a first meetup',
+      }
+    }
+  }
+}
+
 export default MeetupDetails;
 
