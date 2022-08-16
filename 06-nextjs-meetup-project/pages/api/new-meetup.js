8b163bb00e81b0393bc24f381ba1f1ee07b19e66
Alan Leverenz <awleverenz@aol.com>
2022-08-16 18:26:58 -0400

    inserted MongoClient

--- 06-nextjs-meetup-project/pages/api/new-meetup.js
+++ 06-nextjs-meetup-project/pages/api/new-meetup.js
@@ -1,3 +1,5 @@
+import { MongoClient } from 'mongodb';
+
 // server-side code that is not exposed to the client
 // triggered when a route is used
 // only POST requests trigger /api/new-meetup
@@ -8,6 +10,7 @@ function handler(req, res) {
 
     const { title, image, address, description } = data;
 
+    MongoClient.connect('mongosh "mongodb+srv://cluster0.bk62d2e.mongodb.net/meetups" --apiVersion 1 --username alanleverenz');
 
   }
 }
