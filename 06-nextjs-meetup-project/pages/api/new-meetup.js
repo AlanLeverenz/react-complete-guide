abb3241c3ccec57bf414f0bb4326977313e302e1
Alan Leverenz <awleverenz@aol.com>
2022-08-17 10:58:41 -0400

    tried using MongoDB code but still getting 404 error

--- 06-nextjs-meetup-project/pages/api/new-meetup.js
+++ 06-nextjs-meetup-project/pages/api/new-meetup.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 // server-side code that is not exposed to the client
 // triggered when a route is used
@@ -7,28 +7,45 @@ import { MongoClient } from 'mongodb';
 async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
+    // const { title, image, address, description } = data;
 
-    const { title, image, address, description } = data;
+    const uri = "mongodb+srv://alanleverenz:Newlife70MDB@cluster0.bk62d2e.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-    console.log(title, image, address, description);
-
-    // const client = await MongoClient.connect('mongodb+srv://alanleverenz:Newlife70MDB@cluster0.bk62d2e.mongodb.net/meetups?retryWrites=true&2=majority');
-
-    const client = await MongoClient.connect('mongosh "mongodb+srv://cluster0.bk62d2e.mongodb.net/meetups" --apiVersion 1 --username alanleverenz');
+    // const client = await MongoClient.connect("mongodb+srv://alanleverenz:Newlife70MDB@cluster0.bk62d2e.mongodb.net/?retryWrites=true&w=majority");
 
     const db = client.db();
-
     const meetupsCollection = db.collection('meetups');
-
     const result = await meetupsCollection.insertOne(data);
-
     console.log(result);
-
     client.close();
-
     res.status(201).json({ message: 'Meetup inserted!' });
+
+    // MongoDB full code 
+
+    // const { MongoClient, ServerApiVersion } = require('mongodb');
+    // const uri = "mongodb+srv://alanleverenz:Newlife70MDB@cluster0.bk62d2e.mongodb.net/?retryWrites=true&w=majority";
+    // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    // client.connect(err => {
+    //   const collection = client.db("test").collection("devices");
+    //   // perform actions on the collection object
+    //   client.close();
+    // });
+    // res.status(201).json({ message: 'Meetup inserted!' });
 
   }
 }
 
 export default handler;
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://alanleverenz:<password>@cluster0.bk62d2e.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+    // const client = await MongoClient.connect('mongosh "mongodb+srv://cluster0.bk62d2e.mongodb.net/meetups" --apiVersion 1 --username alanleverenz');
