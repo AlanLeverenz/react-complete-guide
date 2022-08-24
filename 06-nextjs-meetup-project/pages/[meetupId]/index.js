a29d132435f19da1a3ccbf30887db7fda346ccec
Alan Leverenz <awleverenz@aol.com>
2022-08-24 14:35:48 -0400

    updating index from [meetupid]

--- 06-nextjs-meetup-project/pages/[meetupId]/index.js
+++ 06-nextjs-meetup-project/pages/[meetupId]/index.js
@@ -20,29 +20,17 @@ export async function getStaticPaths() {
   // false fallback only lets server render listed paths
 
   const client = await MongoClient.connect(`mongodb+srv://${keys.MDB_username}:${keys.MDB_password}@cluster0.bk62d2e.mongodb.net/?retryWrites=true&w=majority`);
-
   const db = client.db();
   const meetupsCollection = db.collection('meetups');
-
   // get all documents but only the _id field
   const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  client.close();
 
   return {
     fallback: false,
-    paths: meetups.map(meetup => ({ params: { meetupId: meetup._id.toString } }))
-
-    // [
-    //   {
-    //     params: {
-    //       meetupId: 'm1',
-    //     },
-    //   },
-    //   {
-    //     params: {
-    //       meetupId: 'm2',
-    //     },
-    //   },
-    // ],
+    paths: meetups.map(meetup => ({
+      params: { meetupId: meetup._id.toString },
+    }))
   };
 }
 
@@ -52,18 +40,20 @@ export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
 
   console.log(meetupId);
+  const client = await MongoClient.connect(`mongodb+srv://${keys.MDB_username}:${keys.MDB_password}@cluster0.bk62d2e.mongodb.net/?retryWrites=true&w=majority`);
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({ _id: meetupId });
+
+  client.close();
 
   return {
     props: {
-      meetupData: {
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-        id: meetupId,
-        title: 'A First Meetup',
-        address: 'New Town Hall, Old Town of Munich',
-        description: 'This is a first meetup',
-      }
-    }
-  }
+      meetupData: selectedMeetup,
+    },
+  };
 }
 
 export default MeetupDetails;
--- 06-nextjs-meetup-project/pages/index.js
+++ 06-nextjs-meetup-project/pages/index.js
@@ -4,22 +4,22 @@ import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
 import keys from '../keys';
 
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'New Town Hall, Old Town of Munich',
-    description: 'This is a first meetup!'
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image: 'https://pix10.agoda.net/geo/city/318/1_318_02.jpg?ca=6&ce=1&s=1920x822',
-    address: 'New York City',
-    description: 'This is a second meetup!'
-  }
-]
+// const DUMMY_MEETUPS = [
+//   {
+//     id: 'm1',
+//     title: 'A First Meetup',
+//     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+//     address: 'New Town Hall, Old Town of Munich',
+//     description: 'This is a first meetup!'
+//   },
+//   {
+//     id: 'm2',
+//     title: 'A Second Meetup',
+//     image: 'https://pix10.agoda.net/geo/city/318/1_318_02.jpg?ca=6&ce=1&s=1920x822',
+//     address: 'New York City',
+//     description: 'This is a second meetup!'
+//   }
+// ]
 
 function HomePage(props) {
 
