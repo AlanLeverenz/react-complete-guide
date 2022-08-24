370ee98665d355099bedc6c46a8830ba098ef302
Alan Leverenz <awleverenz@aol.com>
2022-08-24 14:47:15 -0400

    edited [meetupid] index to get item from monogdb

--- 06-nextjs-meetup-project/pages/[meetupId]/index.js
+++ 06-nextjs-meetup-project/pages/[meetupId]/index.js
@@ -1,23 +1,21 @@
-import { MongoClient } from 'monogdb';
+import { MongoClient, ObjectId } from 'monogdb';
 import keys from '../../keys';
 
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
-      title='A First Meetup'
-      address='New Town Hall, Old Town of Munich'
-      description='This is a first meetup!'
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 
+// is run before the first function
 export async function getStaticPaths() {
-  // must include all possible id objects
-  // true fallback lets server render paths dynamically if not listed here
-  // false fallback only lets server render listed paths
 
   const client = await MongoClient.connect(`mongodb+srv://${keys.MDB_username}:${keys.MDB_password}@cluster0.bk62d2e.mongodb.net/?retryWrites=true&w=majority`);
   const db = client.db();
@@ -40,18 +38,25 @@ export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
 
   console.log(meetupId);
+
   const client = await MongoClient.connect(`mongodb+srv://${keys.MDB_username}:${keys.MDB_password}@cluster0.bk62d2e.mongodb.net/?retryWrites=true&w=majority`);
   const db = client.db();
 
   const meetupsCollection = db.collection('meetups');
 
-  const selectedMeetup = await meetupsCollection.findOne({ _id: meetupId });
+  const selectedMeetup = await meetupsCollection.findOne({ _id: ObjectId(meetupId) });
 
   client.close();
 
   return {
     props: {
-      meetupData: selectedMeetup,
+      meetupData: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
+      },
     },
   };
 }
