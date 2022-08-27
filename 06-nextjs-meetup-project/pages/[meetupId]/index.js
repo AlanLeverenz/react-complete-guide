7a613b2ada0ed764b82f24ed0e54c409d5789583
Alan Leverenz <52021100+AlanLeverenz@users.noreply.github.com>
2022-08-26 22:30:27 -0400

    updated meetupId index file

--- 06-nextjs-meetup-project/pages/[meetupId]/index.js
+++ 06-nextjs-meetup-project/pages/[meetupId]/index.js
@@ -1,17 +1,16 @@
-import { MongoClient, ObjectId } from 'monogdb';
+import { MongoClient, ObjectId } from 'mongodb';
 import keys from '../../keys';
-import Head from 'next/head';
-import MeetupDetail from '../../components/meetups/MeetupDetail';
 import { Fragment } from 'react';
+import Head from 'next/head';
+
+import MeetupDetail from '../../components/meetups/MeetupDetail';
 
 function MeetupDetails(props) {
   return (
     <Fragment>
       <Head>
         <title>{props.meetupData.title}</title>
-        <meta
-          name="description"
-          content={props.meetupData.description} />
+        <meta name='description' content={props.meetupData.description} />
       </Head>
       <MeetupDetail
         image={props.meetupData.image}
@@ -23,37 +22,44 @@ function MeetupDetails(props) {
   );
 }
 
+
+
 // is run before the first function
 export async function getStaticPaths() {
-
-  const client = await MongoClient.connect(`mongodb+srv://${keys.MDB_username}:${keys.MDB_password}@cluster0.bk62d2e.mongodb.net/?retryWrites=true&w=majority`);
+  const client = await MongoClient.connect(
+    `mongodb+srv://${keys.MDB_username}:${keys.MDB_password}@cluster0.bk62d2e.mongodb.net/?retryWrites=true&w=majority`
+  );
   const db = client.db();
+
   const meetupsCollection = db.collection('meetups');
-  // get all documents but only the _id field
+
   const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
   client.close();
 
   return {
-    fallback: false,
-    paths: meetups.map(meetup => ({
-      params: { meetupId: meetup._id.toString },
-    }))
+    fallback: 'blocking',
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
 export async function getStaticProps(context) {
-  // fetch data from the server for a single meetup
+  // fetch data for a single meetup
 
   const meetupId = context.params.meetupId;
 
-  console.log(meetupId);
-
-  const client = await MongoClient.connect(`mongodb+srv://${keys.MDB_username}:${keys.MDB_password}@cluster0.bk62d2e.mongodb.net/?retryWrites=true&w=majority`);
+  const client = await MongoClient.connect(
+    `mongodb+srv://${keys.MDB_username}:${keys.MDB_password}@cluster0.bk62d2e.mongodb.net/?retryWrites=true&w=majority`
+  );
   const db = client.db();
 
   const meetupsCollection = db.collection('meetups');
 
-  const selectedMeetup = await meetupsCollection.findOne({ _id: ObjectId(meetupId) });
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
 
   client.close();
 
@@ -71,4 +77,6 @@ export async function getStaticProps(context) {
 }
 
 export default MeetupDetails;
+
+
 
